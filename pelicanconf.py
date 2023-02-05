dc51fb980cf1b85bbc2ca5ae602014f66a9cb49e
Vivek Malhotra <malhotra@ualberta.ca>
2023-02-05 15:07:20 -0700

    update pelicanconf.py

--- pelicanconf.py
+++ pelicanconf.py
@@ -10,7 +10,7 @@ DEFAULT_LANG = 'English'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
+# CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
@@ -26,6 +26,8 @@ SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 10
+
+THEME = 'templates/mytheme'
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
